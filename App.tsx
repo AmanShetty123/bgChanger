ca0a5231b99be636afef2a743092a9463a278dce
AmanShetty123 <shettyaman016@gmail.com>
2024-06-17 13:05:56 +0530

    Simple Background Changer APP

--- App.tsx
+++ App.tsx
@@ -1,117 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
 } from 'react-native';
+import React, {useState} from 'react';
+import {Circle, Square} from 'react-native-shape';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [bgCircle, setBgCircle] = useState('#fff');
+  const [bgsmallCircle, setBgSmallCircle] = useState('#fff');
+  const [bgSquare, setBgSquare] = useState('#fff');
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF';
+    let color1 = '#';
+    for (let i = 0; i < 6; i++) {
+      color1 += hexRange[Math.floor(Math.random() * hexRange.length)];
+    }
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    let color2 = '#';
+    for (let i = 0; i < 6; i++) {
+      color2 += hexRange[Math.floor(Math.random() * hexRange.length)];
+    }
+    let color3 = '#';
+    for (let i = 0; i < 6; i++) {
+      color3 += hexRange[Math.floor(Math.random() * hexRange.length)];
+    }
+    setBgCircle(color1);
+    setBgSmallCircle(color2);
+    setBgSquare(color3);
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <StatusBar backgroundColor={'#000'} />
+      <View style={styles.container}>
+        <View style={[styles.circle]}>
+          <Circle scale={4} color={bgCircle} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <View style={styles.btnAndCircle}>
+          <View style={styles.btn}>
+            <TouchableOpacity onPress={generateColor}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: 'bold',
+                }}>
+                Press Me
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Circle scale={1} color={bgsmallCircle} />
+        </View>
+        <View style={styles.square}>
+          <Square scale={3} color={bgSquare} />
+        </View>
+      </View>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  circle: {
+    marginLeft: '25%',
+    marginTop: '25%',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  btnAndCircle: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginTop: '30%',
   },
-  highlight: {
-    fontWeight: '700',
+  btn: {
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderRadius: 12,
+    backgroundColor: '#6a14bd',
+  },
+  square: {
+    marginLeft: '50%',
+    marginTop: '30%',
   },
 });
 
